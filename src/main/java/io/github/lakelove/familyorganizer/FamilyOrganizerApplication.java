f7a0389bddfcfa7d653f6aaa9d8f819c55d394ac
Lake Greene <lakegreene@yahoo.com>
2021-10-18 18:08:48 -0400

    Add project files from Spring Initializr

---
+++ src/main/java/io/github/lakelove/familyorganizer/FamilyOrganizerApplication.java
@@ -0,0 +1,13 @@
+package io.github.lakelove.familyorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/lakelove/familyorganizer/FamilyOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lakelove.familyorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
